84527f9ba71a2be5f0c0c909fa9223d3389a6795
Daffa Rizki <daffa.prasetia@indocyber.id>
2023-07-26 08:46:34 +0700

    initial commit

---
+++ src/main/java/com/Asset/bgrillservice/BgrillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Asset.bgrillservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgrillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgrillServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Asset/bgrillservice/BgrillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Asset.bgrillservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgrillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
